a31d9d0daf7cd28b10feea0a7b427b470da722b3
maxFischer42 <maxfischer072@gmail.com>
2020-07-05 18:17:48 -0700

    Add AttackData objects

---
+++ Assets/Attackdata/AttackData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AttackData : ScriptableObject {
+    public float attackCooldown;
+    public float followupCount;
+}
